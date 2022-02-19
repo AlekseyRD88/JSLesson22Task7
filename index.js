e8103a9b6633abadcd68775201a436a480aa810b
= <neymegen50@gmail.com>
2022-02-19 14:40:02 +0200

    message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const buttonElem = document.querySelectorAll('.btn');
 for (let i = 0; i < buttonElem.length; i++) {
   let button = buttonElem[i];
   button.addEventListener('click', function handleClick(event) {
-     console.log(event.target.textContent);
-  }
+    console.log(event.target.textContent);
+  });
 }
