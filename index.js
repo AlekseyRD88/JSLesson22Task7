bdc15cf1824e18bffcbc75b73f3c9df3f81f53bc
= <neymegen50@gmail.com>
2022-02-19 23:06:02 +0200

    message2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const buttonElem = document.querySelectorAll('.btn');
 for (let i = 0; i < buttonElem.length; i++) {
   let button = buttonElem[i];
   function handleClick(event) {
-    console.log(event.target.textContent);
+    const elemEvent = event.target;
+    console.log(elemEvent.textContent);
   }
   button.addEventListener('click', handleClick);
 }
