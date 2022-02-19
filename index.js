e9009ab7f884d1ff31c01277a9e5e870d3da6467
= <neymegen50@gmail.com>
2022-02-19 14:43:00 +0200

    message2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const buttonElem = document.querySelectorAll('.btn');
 for (let i = 0; i < buttonElem.length; i++) {
   let button = buttonElem[i];
-  button.addEventListener('click', function handleClick(event) {
+  function handleClick(event) {
     console.log(event.target.textContent);
-  });
+  }
+  button.addEventListener('click', handleClick);
 }
