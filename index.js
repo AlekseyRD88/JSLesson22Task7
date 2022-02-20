ed62b7751914737f5c1f7abeb2c3655203067cbb
= <neymegen50@gmail.com>
2022-02-20 21:57:41 +0200

    message4

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const buttonElem = document.querySelectorAll('.btn');
 for (let i = 0; i < buttonElem.length; i++) {
   let button = buttonElem[i];
-  function handleClick(event) {
-    const elemEvent = event.target;
-    console.log(elemEvent.textContent);
-  }
   button.addEventListener('click', handleClick);
+}
+function handleClick(event) {
+  const elemEvent = event.target;
+  console.log(elemEvent.textContent);
 }
